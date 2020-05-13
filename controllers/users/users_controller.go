7c5def99f549827ab32e10c11999ddbdf3b61676
Eduardo Montejo <eduardo.montejo@ceiba.com.co>
2020-05-13 14:37:47 -0500

    dao structure was implemented

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -2,6 +2,7 @@ package users
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/EduardoMontejo/bookstore_users-api/domain/users"
 	"github.com/EduardoMontejo/bookstore_users-api/services"
@@ -16,6 +17,7 @@ func CreateUser(c *gin.Context) {
 		c.JSON(restErr.Status, restErr)
 		return
 	}
+
 	result, saveErr := services.CreateUser(user)
 	if saveErr != nil {
 		c.JSON(saveErr.Status, saveErr)
@@ -25,5 +27,16 @@ func CreateUser(c *gin.Context) {
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+	c.JSON(http.StatusOK, user)
 }
