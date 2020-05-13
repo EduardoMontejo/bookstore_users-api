0c4feecf4c681144ca4c3c042b9b8c2807d6dfc4
Eduardo Montejo <eduardo.montejo@ceiba.com.co>
2020-05-13 17:31:16 -0500

    variables for datasource was modified

--- datasources/mysql/users_db/users_db.go
+++ datasources/mysql/users_db/users_db.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -18,10 +19,10 @@ const (
 var (
 	Client *sql.DB
 
-	username := os.Getenv(mysql_users_username)
-	password := os.Getenv(mysql_users_password)
-	host := os.Getenv(mysql_users_host)
-	schema := os.Getenv(mysql_users_schema)
+	username = os.Getenv(mysql_users_username)
+	password = os.Getenv(mysql_users_password)
+	host     = os.Getenv(mysql_users_host)
+	schema   = os.Getenv(mysql_users_schema)
 )
 
 func init() {
