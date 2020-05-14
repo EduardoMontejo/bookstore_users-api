d4ac5058cda099783c9b5cebed67ccd9eba9b2b0
Eduardo Montejo <eduardo.montejo@ceiba.com.co>
2020-05-14 08:37:57 -0500

    insert into users table was implemented

--- datasources/mysql/users_db/users_db.go
+++ datasources/mysql/users_db/users_db.go
@@ -4,30 +4,29 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-const (
-	mysql_users_username = "mysql_users_username"
-	mysql_users_password = "mysql_users_password"
-	mysql_users_host     = "mysql_users_host"
-	mysql_users_schema   = "mysql_users_schema"
-)
+// const (
+// 	mysql_users_username = "mysql_users_username"
+// 	mysql_users_password = "mysql_users_password"
+// 	mysql_users_host     = "mysql_users_host"
+// 	mysql_users_schema   = "mysql_users_schema"
+// )
 
 var (
 	Client *sql.DB
 
-	username = os.Getenv(mysql_users_username)
-	password = os.Getenv(mysql_users_password)
-	host     = os.Getenv(mysql_users_host)
-	schema   = os.Getenv(mysql_users_schema)
+	// username = os.Getenv(mysql_users_username)
+	// password = os.Getenv(mysql_users_password)
+	// host     = os.Getenv(mysql_users_host)
+	// schema   = os.Getenv(mysql_users_schema)
 )
 
 func init() {
 	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
-		username, password, host, schema,
+		"root", "12345", "127.0.0.1:3306", "users_db",
 	)
 	var err error
 	Client, err = sql.Open("mysql", dataSourceName)
--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,10 +2,16 @@ package users
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/EduardoMontejo/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/EduardoMontejo/bookstore_users-api/utils/date_utils"
 	"github.com/EduardoMontejo/bookstore_users-api/utils/errors"
+)
+
+const (
+	indexUniqueEmail = "email_UNIQUE"
+	queryInsertUser  = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
 )
 
 var (
@@ -32,15 +38,30 @@ func (user *User) Get() *errors.RestErr {
 }
 
 func (user *User) Save() *errors.RestErr {
-	current := usersDB[user.Id]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
+	defer stmt.Close()
+
 	user.DateCreated = date_utils.GetNowString()
 
-	usersDB[user.Id] = user
+	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if err != nil {
+		if strings.Contains(err.Error(), indexUniqueEmail) {
+			return errors.NewBadRequestError(
+				fmt.Sprintf("email %s already exists", user.Email))
+		}
+		return errors.NewInternalServerError(
+			fmt.Sprintf("error when trying to save user: %s", err.Error()))
+	}
+
+	userId, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError(
+			fmt.Sprintf("error when trying to save user: %s", err.Error()))
+	}
+
+	user.Id = userId
 	return nil
 }
